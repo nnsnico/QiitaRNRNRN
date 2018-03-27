4428d100c4d93aa0965212cd92dff372ad95599c
nnsnico5214 <yuji.jampan.blue@gmail.com>
2018-03-28 01:06:05 +0900

    Add store, action, and saga

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,15 @@
 const GO_DETAIL = 'GO_DETAIL';
+const LOAD_TOPICS = 'LOAD_TOPICS';
 
 export function goDetail() {
   return {
     type: GO_DETAIL,
+  };
+}
+
+export function loadTopics(topics) {
+  return {
+    type: LOAD_TOPICS,
+    topics,
   };
 }
